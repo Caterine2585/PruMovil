b0afbde7805a081a7330f9058662c6da3c2f31f9
Kevin Morales <kevinchomorave@gmail.com>
2025-11-06 16:00:04 -0500

    Subir vista Productos con POST y GET

--- mimovil4/app/src/main/java/com/example/mimovil/api/ApiServiceKotlin.kt
+++ mimovil4/app/src/main/java/com/example/mimovil/api/ApiServiceKotlin.kt
@@ -16,7 +16,6 @@ interface ApiServiceKotlin {
     @POST("RegistraC")
     fun crearCliente(@Body persona: Cliente): Call<ResponseBody>
 
-
     @PUT("ActualizarC/{documento}")
     fun actualizarCliente(
         @Path("documento") documento: String,
@@ -26,9 +25,15 @@ interface ApiServiceKotlin {
     @DELETE("EliminarC/{documento}")
     fun eliminarCliente(@Path("documento") documento: String): Call<ResponseBody>
 
-
     // POST Empleados
     @Headers("Content-Type: application/json")
     @POST("EmpleadoRegistro") // cámbialo si tu endpoint real es otro
     fun crearEmpleado(@Body empleado: Empleado): Call<ResponseBody>
+
+    //Productos
+    @GET("Productos")
+    fun getProducto(): Call<List<String>>
+    @Headers("Content-Type: application/json")
+    @POST("crearProducto")
+    fun crearProducto(@Body producto: Producto): Call<ResponseBody>
 }
